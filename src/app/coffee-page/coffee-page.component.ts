c6282082aa59618592e2917f4abfcf3d24cc622e
Jummie <jummiejk21@email.com>
2024-04-19 16:08:09 +0100

    Added filters to the products

--- src/app/coffee-page/coffee-page.component.ts
+++ src/app/coffee-page/coffee-page.component.ts
@@ -9,6 +9,13 @@ import { coffeeData } from '../shared/data/coffee-data';
 })
 export class CoffeePageComponent {
   coffeeProducts = coffeeData;
-  
+  filter: string = '';
+
+// Filter products based on the product name
+// If filter is empty display all the products if not display items with the filtered name
+  getfilterProducts(){
+    return this.filter === '' ? this.coffeeProducts
+    :this.coffeeProducts.filter((product) => product.coffeeName === this.filter)
+  }
 
 }
