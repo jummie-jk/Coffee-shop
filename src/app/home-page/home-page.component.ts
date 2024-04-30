f00a844e3eb5a76d11387ea226a0291925dd75f8
jummie-jk <jummiejk21@gmail.com>
2024-04-30 13:05:41 +0100

    Added more UI styles to the home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../components/buttons/primary-button.component';
 import { TopbarComponent } from '../components/topbar/topbar.component';
+import { SmallButtonComponent } from '../components/buttons/small-button.component';
+import { coffeeData } from '../shared/data/coffee-data';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
     standalone: true,
-    imports: [TopbarComponent, PrimaryButtonComponent]
+    imports: [TopbarComponent, PrimaryButtonComponent, SmallButtonComponent, CommonModule]
 })
 export class HomePageComponent {
   start: boolean = false
   text: string = "Coffee"
+  coffeeProducts = coffeeData;
 
 
 
