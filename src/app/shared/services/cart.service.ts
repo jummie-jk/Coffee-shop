e578e285ad8d73bc4f673cad2c83f0306f6846e9
jummie-jk <jummiejk21@gmail.com>
2024-07-15 19:26:25 +0100

    cleaned up cart page

--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -4,42 +4,45 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  cart: ICoffeeData[] = []
-  baseUrl: string = "http://localhost:5000"
-  baseUrls: string = 'https://beanscene-coffeeshop-default-rtdb.firebaseio.com/cartProducts'
+  cart: ICoffeeData[] = [];
+  baseUrl: string = 'http://localhost:5000';
+  baseUrls: string =
+    'https://beanscene-coffeeshop-default-rtdb.firebaseio.com/cartProducts';
 
-  constructor(private httpClient: HttpClient) { }
-  
-  add(product: ICoffeeData){
-    this.cart.push(product);
-    this.httpClient.post('https://beanscene-coffeeshop-default-rtdb.firebaseio.com/cartProducts.json', product).subscribe(()=> {
-      console.log(`product ${product.coffeeName} added to cart`)
-    })
-  }
-  addToCart(product: ICartDetails){
-    // this.cart.push(product);
-    this.httpClient.post(`${this.baseUrl}/cart/add`, product).subscribe(()=> {
-      console.log(`product ${product.productId} added to cart`)
-    })
+  constructor(private httpClient: HttpClient) {}
+
+  addToCart(product: ICartDetails) {
+    this.httpClient.post(`${this.baseUrl}/cart/add`, product).subscribe(() => {
+      console.log(`product ${product.productId} added to cart`);
+    });
   }
   removeCart(productId: number): Observable<any> {
     const url = `${this.baseUrl}/cart/remove`;
     return this.httpClient.post(url, { productId });
   }
 
-  getCart(){
-    return this.httpClient.get(`${this.baseUrl}/cart/`)
+  getCart() {
+    return this.httpClient.get(`${this.baseUrl}/cart/`);
   }
-  // removeCart(id: number): Observable<any> {
-  //   return this.httpClient.delete(`${this.baseUrl}/cart/remove`)
-  // }
+
   removeProductFromCart(id: string): Observable<any> {
-  return this.httpClient.delete(`https://coffee-shop-86d24.firebaseio.com/cartProducts/${id}`);
+    return this.httpClient.delete(
+      `https://coffee-shop-86d24.firebaseio.com/cartProducts/${id}`
+    );
   }
-  // removeFromCart(id: number): Observable<any> {
-  //   return this.httpClient.delete(`${this.baseUrl}/cartProducts/${id}`);
-  // }
+  //
+  add(product: ICoffeeData) {
+    this.cart.push(product);
+    this.httpClient
+      .post(
+        'https://beanscene-coffeeshop-default-rtdb.firebaseio.com/cartProducts.json',
+        product
+      )
+      .subscribe(() => {
+        console.log(`product ${product.coffeeName} added to cart`);
+      });
+  }
 }
